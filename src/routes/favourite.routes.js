289f1d4291923de344ff8a306c130100fbed1996
harshaprada04 <prada.harsha4@gmail.com>
2025-07-08 15:02:44 +0530

    Add auth middleware and refactor routes for better organization

--- src/routes/favourite.routes.js
+++ src/routes/favourite.routes.js
@@ -1,11 +1,17 @@
 import { Router } from "express";
-import {favouriteController, favouriteList, removeFromFavourites} from "../controller/favourite.controller.js";
-import authMiddleWare from "../middlewares/authMiddleWare.js";
+import {
+  favouriteController,
+  favouriteList,
+  removeFromFavourites,
+} from "../controller/favourite.controller.js";
+import authMiddleWare from "../middleWare/authMiddleWare.js";
 
 const favRouter = Router();
 
-favRouter.route("/favourites").post(authMiddleWare,favouriteController);
-favRouter.route("/favouritesList").get(authMiddleWare,favouriteList);
-favRouter.route("/favourites/:videoId").delete(authMiddleWare,removeFromFavourites);
+favRouter.route("/favourites").post(authMiddleWare, favouriteController);
+favRouter.route("/favouritesList").get(authMiddleWare, favouriteList);
+favRouter
+  .route("/favourites/:videoId")
+  .delete(authMiddleWare, removeFromFavourites);
 
 export default favRouter;
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,14 +1,20 @@
 import { Router } from "express";
-import {registerUser, loginUser, currentUserData} from "../controller/user.controller.js";
-import authMiddleWare from "../middlewares/authMiddleWare.js";
+import {
+  registerUser,
+  loginUser,
+  currentUserData,
+} from "../controller/user.controller.js";
+
 
 const router = Router();
 
-router.route("/register").post(registerUser)
+router.route("/register").post(registerUser);
 
 router.route("/login").post(loginUser);
 
 //secured routes
-router.route("/userData").get(authMiddleWare,currentUserData);
+import authMiddleWare from "../middleWare/authMiddleWare.js";
+
+router.route("/userData").get(authMiddleWare, currentUserData);
 
 export default router;
